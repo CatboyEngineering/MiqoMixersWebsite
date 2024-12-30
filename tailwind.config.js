300717892d498498b9a7c471b34d1184b2af0c14
Catboy Engineering <catboy.engineering@pm.me>
2024-12-29 18:58:14 -0700

    Adjust dark theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,14 @@ module.exports = {
         mmDarkerSand: '#c1b699',
         mmOrange: '#FD8B51',
         mmRust: '#CB6040',
-        mmDarkRust: '#a04e35'
+        mmDarkRust: '#a04e35',
+        mmClubPurple: '#2E1C35',
+        mmClubLavender: '#554971',
+        mmClubSpace: '#383348',
+        mmClubDeepSpace: '#2B2934',
+        mmClubBlack: '#1D1E20',
+        mmClubBlue: '#121A2B',
+        mmCyan: '#18848e'
       },
       fontFamily: {
         lilitaOne: ['Lilita One', 'sans-serif'],
